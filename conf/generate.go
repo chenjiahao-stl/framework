15cf6222613298789038fcb86a0b7fd0002c4143
conor.chen <conor.chen@isyun.co>
2024-11-23 10:33:54 +0800

    feat: init

---
+++ conf/generate.go
@@ -0,0 +1,3 @@
+package conf
+
+//go:generate protoc --experimental_allow_proto3_optional  -I ./  --go_out=. ./common.proto
---
+++ kratos-v1/config/config.go
@@ -0,0 +1,6 @@
+package config
+
+// InitConfig 初始化nacos上的配置或者环境变量的配置到指定的对象中/*
+func InitConfig() {
+
+}
